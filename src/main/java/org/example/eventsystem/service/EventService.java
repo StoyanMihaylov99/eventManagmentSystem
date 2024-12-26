b53ed64c9769543327ea174b085fb2f974f0f111
stoyanmihaylov <stoqn.business@gmail.com>
2024-12-26 23:04:54 +0200

    creating modelMapper

--- src/main/java/org/example/eventsystem/service/EventService.java
+++ src/main/java/org/example/eventsystem/service/EventService.java
@@ -1,14 +1,17 @@
 package org.example.eventsystem.service;
 
 import org.example.eventsystem.repository.EventRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EventService {
     private EventRepository eventRepository;
+    private ModelMapper modelMapper;
 
-    public EventService(EventRepository eventRepository) {
+    public EventService(EventRepository eventRepository, ModelMapper modelMapper) {
         this.eventRepository = eventRepository;
+        this.modelMapper = modelMapper;
     }
 
 
