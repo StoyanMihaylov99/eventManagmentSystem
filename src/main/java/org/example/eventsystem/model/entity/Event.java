73f149253f53808149a41270293dbfb85f482dfd
stoyanmihaylov <stoqn.business@gmail.com>
2024-12-26 22:58:14 +0200

    Adding models

--- src/main/java/org/example/eventsystem/model/entity/Event.java
+++ src/main/java/org/example/eventsystem/model/entity/Event.java
@@ -1,12 +1,30 @@
 package org.example.eventsystem.model.entity;
-import org.example.eventsystem.utils.Category;
-import java.time.LocalDateTime;
 
+import jakarta.persistence.*;
+import org.example.eventsystem.utils.Category;
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name = "events")
 public class Event {
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    @Column(updatable = false, nullable = false)
+    private String id;
+    @Column(name = "title")
     private String title;
-    private LocalDateTime DateTime;
+    @Column(name = "date_time")
+    private String dateTime;
+    @Enumerated(EnumType.STRING)
     private Category category;
+    @Column(name = "total_seats")
     private int totalSeats;
+    @Column(name = "available_seats")
     private int availableSeats;
-    private User lead;
+    @ManyToOne
+    private User admin;
+
+    public Event() {
+    }
 }
--- src/main/java/org/example/eventsystem/model/entity/User.java
+++ src/main/java/org/example/eventsystem/model/entity/User.java
@@ -1,4 +1,24 @@
 package org.example.eventsystem.model.entity;
 
+import jakarta.persistence.*;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(generator = "uuid-string")
+    private String id;
+    @Column(name = "user_name")
+    private String userName;
+    @Column(name = "password")
+    private String password;
+    @Column(name = "created")
+    private String created;
+    @OneToMany(mappedBy = "admin", cascade = CascadeType.ALL)
+    private Set<Event> events;
+
+    public User() {
+    }
 }
