2d15e384614d0cb978e68d44ac96cb1e3c46c3e5
stoyanmihaylov <stoqn.business@gmail.com>
2024-12-28 11:55:56 +0200

    creating CRUD operations for events

--- src/main/java/org/example/eventsystem/repository/EventRepository.java
+++ src/main/java/org/example/eventsystem/repository/EventRepository.java
@@ -1,9 +1,19 @@
 package org.example.eventsystem.repository;
 
 import org.example.eventsystem.model.entity.Event;
+import org.example.eventsystem.utils.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface EventRepository extends JpaRepository<Event,String> {
+
+    Optional<List<Event>> findEventsByCategory(Category category);
+    Optional<Event> findByTitle(String title);
+    Optional<Event> findById(String id);
+    void deleteById(String id);
+
 }
